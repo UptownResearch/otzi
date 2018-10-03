3531a9711eeb9655f19b70dc4b41f635d0a857f5
aniemerg <niemerg@gmail.com>
2018-10-03 09:45:44 -0500

    added code to find modules for import

--- market_maker/auth/__init__.py
+++ market_maker/auth/__init__.py
@@ -1,3 +1,10 @@
+# Find code directory relative to our directory
+from os.path import dirname, abspath, join
+import sys
+THIS_DIR = dirname(__file__)
+CODE_DIR = abspath(join(THIS_DIR, '..', '..' ))
+sys.path.append(CODE_DIR)
+
 from market_maker.auth.AccessTokenAuth import *
 from market_maker.auth.APIKeyAuth import *
 from market_maker.auth.APIKeyAuthWithExpires import *
