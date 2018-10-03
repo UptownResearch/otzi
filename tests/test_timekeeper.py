d56e32d24f022a073e68729d1c165d4a0e2d4442
aniemerg <niemerg@gmail.com>
2018-10-03 09:45:12 -0500

    added code to find market_maker modules for import

--- tests/test_timekeeper.py
+++ tests/test_timekeeper.py
@@ -1,5 +1,11 @@
 from unittest.mock import MagicMock, patch
 from unittest import TestCase
+# Find code directory relative to our directory
+from os.path import dirname, abspath, join
+import sys
+THIS_DIR = dirname(__file__)
+CODE_DIR = abspath(join(THIS_DIR, '..' ))
+sys.path.append(CODE_DIR)
 from market_maker.backtest.timekeeper import Timekeeper
 
 
