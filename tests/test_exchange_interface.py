c016e706b2ce0638a23372545cc39fa3f9a9cb4f
aniemerg <niemerg@gmail.com>
2018-10-17 13:34:31 -0500

    tagged some files as deprecated with dep, fixed some unnecessary imports of those files, and added some new test files to shoot for minimal testing of all py files.

---
+++ tests/test_exchange_interface.py
@@ -0,0 +1,63 @@
+import datetime
+import iso8601
+from unittest.mock import MagicMock, patch
+from unittest import TestCase
+import sys
+# Find code directory relative to our directory
+
+
+
+class Test_Exchange_Interface_Module(TestCase):
+
+    def setUp(self):
+        """
+        It's patching time
+        """
+
+        #http://www.voidspace.org.uk/python/mock/examples.html#mocking-imports-with-patch-dict
+        self.settings_mock = MagicMock()
+        #self.settings_mock.settings.BACKTEST.return_value = True
+        #self.settings_mock.settings.paperless = True
+        self.exchange_interface = MagicMock()
+        self.coinbase_book = MagicMock()
+        self.logging = MagicMock()
+        self.log = MagicMock()
+        modules = {
+            'market_maker.settings': self.settings_mock,
+            'market_maker.exchange_interface' : self.exchange_interface,
+            'market_maker.coinbase.order_book' : self.coinbase_book,
+            'market_maker.utils.log' : self.log,
+            'logging' : self.logging
+        }
+        '''
+            To patch:
+            from market_maker import bitmex
+            from market_maker.settings import settings
+            from market_maker.utils import log, constants, errors, math
+            from market_maker.exchange_interface import ExchangeInterface
+            from market_maker.modifiable_settings import ModifiableSettings
+            from market_maker.coinbase.order_book import OrderBook
+
+
+            '''  
+        self.settings_mock.DRY_RUN = False
+        self.settings_mock.BACKTEST = True
+        self.settings_mock.SYMBOL = 'XBTUSD'
+        self.module_patcher = patch.dict('sys.modules', modules)
+        self.module_patcher.start()
+        #from my_module import MyModule
+        #from market_maker.market_maker import OrderManager
+        #self.om = OrderManager()
+
+    def tearDown(self):
+        """
+        Let's clean up
+        """
+
+        self.module_patcher.stop()
+
+
+    def test_module_loads(self):
+        from market_maker.exchange_interface import ExchangeInterface
+        exchange_interface = ExchangeInterface(settings=self.settings_mock)
+        assert exchange_interface is not None
